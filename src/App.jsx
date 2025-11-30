a84eac52928972bac67bc542e2ea857ef9226e23
ALI RAZA <razadeveloper816@gmail.com>
2025-11-30 09:34:35 -0800

    fixing layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import Header from './Component/Header/Header'
 import SideBar from './Component/Sidebar/SideBar'
 export default function App() {
     return (
-        <div>
+        <div style={{display:'flex'}}>
+            <div className="left">
             <SideBar />
+            </div>
+            <div className="right">
             <Header />
+            </div>
         </div>
     )
 }
