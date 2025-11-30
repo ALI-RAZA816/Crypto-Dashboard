635c87c972256f7c38273445a1cf39501c03b47a
ALI RAZA <razadeveloper816@gmail.com>
2025-11-30 09:32:14 -0800

    re-arranging layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css'
 import Header from './Component/Header/Header'
 import SideBar from './Component/Sidebar/SideBar'
 export default function App() {
-  return (
-    <div>
-      <Header/>
-      <SideBar/>
-    </div>
-  )
+    return (
+        <div>
+            <SideBar />
+            <Header />
+        </div>
+    )
 }
--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -3,7 +3,7 @@ import style from '../Header/Header.module.css';
 import { FaRegCircleUser } from "react-icons/fa6";
 export default function Header() {
   return (
-    <header>
+    <header className={style.header}>
         <div className={style.container}>
             <h1>Dashboard</h1>
             <FaRegCircleUser className={style.userIcon} />
