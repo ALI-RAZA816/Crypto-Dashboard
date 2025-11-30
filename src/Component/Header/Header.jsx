9752e853707c849cf9a778c04de74074335c29ea
ALI RAZA <razadeveloper816@gmail.com>
2025-11-30 09:09:44 -0800

    styling header

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from '../Header/Header.module.css';
+import { FaRegCircleUser } from "react-icons/fa6";
+export default function Header() {
+  return (
+    <header>
+        <div className={style.container}>
+            <h1>Dashboard</h1>
+            <FaRegCircleUser className={style.userIcon} />
+        </div>
+    </header>
+  )
+}
