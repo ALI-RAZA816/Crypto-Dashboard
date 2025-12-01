d8dbd4bbbe61b5654e52fc40f6172a5172d00ad9
ALI RAZA <razadeveloper816@gmail.com>
2025-11-30 18:02:17 -0800

    layout component complete

--- src/Component/Pages/Layout/Layout.jsx
+++ src/Component/Pages/Layout/Layout.jsx
@@ -5,6 +5,7 @@ import { LuArrowDownToLine, LuArrowUpToLine } from "react-icons/lu";
 import { FiArrowUpRight } from "react-icons/fi";
 import { TbCirclePlusFilled } from "react-icons/tb";
 import { FaCircleMinus } from "react-icons/fa6";
+import { HiCurrencyDollar } from "react-icons/hi";
 
 
 
@@ -33,18 +34,91 @@ export default function Layout() {
                     </div>
                 </div>
                 <div className="leftBox">
-                    <div className="price">
-                        <span>Current Price</span>
-                        <span><MdCurrencyRupee /> 26,670.25 <p><FiArrowUpRight /> 0.04%</p></span>
+                    <div className="currentPrice">
+                        <div className="price">
+                            <span>Current Price</span>
+                            <span><MdCurrencyRupee /> 26,670.25 <p><FiArrowUpRight /> 0.04%</p></span>
+                        </div>
+                        <div className="btns">
+                            <button><TbCirclePlusFilled /> Buy</button>
+                            <button><FaCircleMinus /> Sell</button>
+                        </div>
                     </div>
-                    <div className="btns">
-                        <button><TbCirclePlusFilled /> Buy</button>
-                        <button><FaCircleMinus /> Sell</button>
+                    <div className="graph">
+                        <div>
+                            <span>1H</span>
+                            <span>1D</span>
+                            <span>1W</span>
+                            <span>1M</span>
+                        </div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                    </div>
+                    <div className="time">
+                        <span>7:15 PM</span>
+                        <span>7:15 PM</span>
+                        <span>7:15 PM</span>
+                        <span>7:15 PM</span>
+                        <span>7:15 PM</span>
                     </div>
                 </div>
-                <div className="rightBox"></div>
-                <div className="bottomleft"></div>
-                <div className="bottomright"></div>
+                <div className="rightBox">
+                    <span>Recent Transactions</span>
+                    <div className="history">
+                        <div className='box box1'>
+                            <div className='text'>
+                                <div>
+                                    <HiCurrencyDollar />
+                                </div>
+                                <div>
+                                    <span>INR Deposit</span>
+                                    <span>2022-06-06 7:06 PM</span>
+                                </div>
+                            </div>
+                            <div className="amount">
+                                <span>+<MdCurrencyRupee />81,123.10</span>
+                            </div>
+                        </div>
+                        <div className='box box2'>
+                            <div className='text'>
+                                <div>
+                                    <HiCurrencyDollar />
+                                </div>
+                                <div>
+                                    <span>INR Deposit</span>
+                                    <span>2022-06-06 7:06 PM</span>
+                                </div>
+                            </div>
+                            <div className="amount">
+                                <span>+<MdCurrencyRupee />81,123.10</span>
+                            </div>
+                        </div>
+                        <div className='box box3'>
+                            <div className='text'>
+                                <div>
+                                    <HiCurrencyDollar />
+                                </div>
+                                <div>
+                                    <span>INR Deposit</span>
+                                    <span>2022-06-06 7:06 PM</span>
+                                </div>
+                            </div>
+                            <div className="amount">
+                                <span>+<MdCurrencyRupee />81,123.10</span>
+                            </div>
+                        </div>
+                    </div>
+                    <button>View All</button>
+                </div>
+                <div className="bottomleft">
+                    <span>Loans</span>
+                    <p>Learn more about Loans – Keep your Bitcoin, access it’s value without selling it</p>
+                </div>
+                <div className="bottomright">
+                    <span>Contact</span>
+                    <p>Learn more about our real estate, mortgage, and  corporate account services</p>
+                </div>
             </div>
         </div>
     )
