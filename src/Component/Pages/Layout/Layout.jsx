58cd9d9b6fc7d8ec94b410e4d9330e5a1ce79be6
ALI RAZA <razadeveloper816@gmail.com>
2025-12-01 08:12:42 -0800

    complete layout

--- src/Component/Pages/Layout/Layout.jsx
+++ src/Component/Pages/Layout/Layout.jsx
@@ -118,6 +118,7 @@ export default function Layout() {
                     <p>Learn more about Loans – Keep your Bitcoin, access it’s value without selling it</p>
                 </div>
                 <div className={style.bottomright}>
+                    <img src="/images/Visual.png" alt="" />
                     <span>Contact</span>
                     <p>Learn more about our real estate, mortgage, and  corporate account services</p>
                 </div>
